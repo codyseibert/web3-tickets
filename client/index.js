1e99290eab7beb8a9b00e97ac4343ae3a13c20c1
Cody Seibert <cseibert113@gmail.com>
2022-01-17 22:00:45 -0500

    workin on ui

---
+++ client/index.js
@@ -0,0 +1,69 @@
+import Web3 from 'web3';
+import configuration from '../build/contracts/Tickets.json';
+import 'bootstrap/dist/css/bootstrap.css';
+import ticketImage from './images/ticket.png';
+
+const createElementFromString = (string) => {
+  const el = document.createElement('div');
+  el.innerHTML = string;
+  return el.firstChild;
+};
+
+const CONTRACT_ADDRESS =
+  configuration.networks['5777'].address;
+const CONTRACT_ABI = configuration.abi;
+
+const web3 = new Web3(
+  Web3.givenProvider || 'http://127.0.0.1:7545'
+);
+const contract = new web3.eth.Contract(
+  CONTRACT_ABI,
+  CONTRACT_ADDRESS
+);
+
+let account;
+
+const accountEl = document.getElementById('account');
+const ticketsEl = document.getElementById('tickets');
+
+const EMPTY_ADDRESS =
+  '0x0000000000000000000000000000000000000000';
+
+const buyTicket = async (ticket) => {
+  await contract.methods
+    .buyTicket(ticket.id)
+    .send({ from: account, value: ticket.price });
+};
+
+const refreshTickets = async () => {
+  ticketsEl.innerHTML = '';
+  for (let i = 0; i < 10; i++) {
+    const ticket = await contract.methods.tickets(i).call();
+    ticket.id = i;
+    if (ticket.owner === EMPTY_ADDRESS) {
+      const ticketEl = createElementFromString(
+        `<div class="ticket card" style="width: 18rem;">
+          <img src="${ticketImage}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Ticket</h5>
+            <p class="card-text">${
+              ticket.price / 1e18
+            } Eth</p>
+            <button class="btn btn-primary">Buy Ticket</button>
+          </div>
+        </div>`
+      );
+      ticketEl.onclick = buyTicket.bind(null, ticket);
+      ticketsEl.appendChild(ticketEl);
+    }
+  }
+};
+
+const main = async () => {
+  const accounts = await web3.eth.requestAccounts();
+  account = accounts[0];
+  accountEl.innerText = account;
+  await refreshTickets();
+};
+
+main();
